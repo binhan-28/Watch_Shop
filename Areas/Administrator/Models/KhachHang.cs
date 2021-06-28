7c72023322e73eb9505779291dd8d4b7c1072787
Bình An <72908406+binhan-28@users.noreply.github.com>
2021-06-28 21:36:24 +0700

    update project

---
+++ Areas/Administrator/Models/KhachHang.cs
@@ -0,0 +1,40 @@
+﻿namespace WatchShop.Areas.Administrator.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("KhachHang")]
+    public partial class KhachHang
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public KhachHang()
+        {
+            HoaDons = new HashSet<HoaDon>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        [Display(Name = "Số điện thoại")]
+        public string DienThoai { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name = "Họ tên KH")]
+        public string HoTenKH { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string EmailKH { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [Display(Name = "Địa chỉ")]
+        public string DiaChiKH { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<HoaDon> HoaDons { get; set; }
+    }
+}
---
+++ Areas/Administrator/Models/SanPham.cs
@@ -0,0 +1,56 @@
+﻿namespace WatchShop.Areas.Administrator.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+    using System.Web.Mvc;
+
+    [Table("SanPham")]
+    public partial class SanPham
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SanPham()
+        {
+            ChiTietHDs = new HashSet<ChiTietHD>();
+        }
+
+        [Key]
+        [Display(Name ="Mã sản phẩm")]
+        public int MaSP { get; set; }
+        [Display(Name = "Tên loại sản phẩm")]
+        public int MaLoaiSP { get; set; }
+        [Display(Name = "Tên thương hiệu")]
+        public int MaThuongHieu { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [Display(Name = "Tên sản phẩm")]
+        public string TenSP { get; set; }
+        [Display(Name = "Mô tả")]
+        [AllowHtml]
+        public string ThongSoKyTHuat { get; set; }
+
+        [Column(TypeName = "money")]
+        [Display(Name = "Đơn giá")]
+        public decimal Gia { get; set; }
+        [Display(Name = "Chiết khấu")]
+        public int Discount { get; set; }
+        [Display(Name = "Số lượng")]
+        public int? SoLuongSP { get; set; }
+        [Display(Name = "Hình ảnh")]
+        public string HinhAnh { get; set; }
+        [Display(Name = "Ngày thêm")]
+        public DateTime? NgayThem { get; set; }
+        [Display(Name = "Tình trạng")]
+        public bool? TinhTrang { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ChiTietHD> ChiTietHDs { get; set; }
+        [Display(Name ="Tên loại sản phẩm")]
+        public virtual LoaiSP LoaiSP { get; set; }
+        [Display(Name = "Tên thương hiệu")]
+        public virtual ThuongHieu ThuongHieu { get; set; }
+    }
+}
